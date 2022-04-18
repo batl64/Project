c947be8da28dd444b9716d1c60088ca88178c7e5
nypzlsta <nyshtoprivet@gmail.com>
2022-04-18 15:38:20 +0300

    console log for alarm
    first attempt for give on api air alarm

--- scrip.js
+++ scrip.js
@@ -1,5 +1,15 @@
-fetch('https://emapa.fra1.cdn.digitaloceanspaces.com/statuses.json',{ 
-    mode: 'no-cors' // 'cors' by default
-}).then(function(response) {
-    // Do something with response
-});
+var ProxyUrl ='https://cors-anywhere.herokuapp.com/'
+    TargetUrl = 'https://alarmmap.online/assets/alerts.json'
+
+fetch(ProxyUrl + TargetUrl)
+    .then(Blob => Blob.json())
+    .then(data=>{
+        console.table(data);
+        document.querySelector("pre").innerHTML = JSON.stringify(data, null, 2);
+        return data;
+    //    console.log(JSON.stringify(data))
+    })   
+    .catch(e => {
+        console.log(e);
+        return e;
+      }); 
